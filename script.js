74981fb371675446327c1944b4f81fc80332511c
daniestrijdom <daansteraan@gmail.com>
2016-09-25 22:16:07 +0200

    some cosmetic changes
    still trying to get margins to work

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
-  var margin = {left: 30,right: 30, top: 30, bottom: 30}
+  var margin = {left: 50,right: 30, top: 30, bottom: 50}
 
   var circleRadius = 3
   var outerHeight = 500
   var outerWidth = 700
   var innerHeight = outerHeight - margin.bottom
   var innerWidth = outerWidth - margin.right
+  console.log(innerWidth)
+  console.log(innerHeight)
+
   var xColumn = 'pH'
   var yColumn = 'alcohol'
   var winetype = 'type'
 
-  $('h2').html("Wine: " + xColumn + ' vs ' + yColumn)
+  $('h2').html("Plot: " + xColumn + ' vs ' + yColumn)
   // Set up the svg space in the DOM
   var svg = d3.select('body').append('svg')
     .attr("height",outerHeight)
@@ -33,18 +36,28 @@ $(document).ready(function() {
       .attr("transform", "rotate(-90)")
       .text(yColumn);
 
-  var g = svg.append('g').attr('tranform', 'translate(400,20)')
   // create the range (pixel length attr) for the plot
   // NOTE: match with size of svg
   var xscale = d3.scale.linear().range([0,innerWidth])
-  var yscale = d3.scale.linear().range([innerHeight,0])
+  var yscale = d3.scale.linear().range([innerHeight, 0])
   var colorscale = d3.scale.linear().range(['rgba(0,255,155,0.3)','rgba(255,0,0,0.3)'])
+
+  var g = svg.append('g').attr('tranform', 'translate('+margin.left+','+margin.top+')')
+  var xAxisG = g.append('g').attr('tranform', 'translate(0,'+innerHeight+')')
+  var yAxisG = g.append('g')
+
+  var xAxis = d3.svg.axis().scale(xscale).orient('bottom')
+  var yAxis = d3.svg.axis().scale(yscale).orient('left')
+
   // main fn to build the plt
   function render(data) {
 
     // set the domain (range of inputs)
     xscale.domain(d3.extent(data, function (d) {return d[xColumn]}))
     yscale.domain(d3.extent(data, function (d) {return d[yColumn]}))
+
+    xAxisG.call(xAxis);
+    yAxisG.call(yAxis);
 
     //Bind data points circle svg element
     var circles = g.selectAll('circle').data(data);
