beacb231e5b1d34b765b77712733820271c1d3fc
daniestrijdom <daansteraan@gmail.com>
2016-09-28 21:58:26 +0200

    axis works... suck it d3
    so... svg groups wants spelling and 'translate' must apparently be
    spelled correctly.

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
 $(document).ready(function() {
 
-  var margin = {left: 30, right: 30, top: 30, bottom: 30}
+  var margin = {left: 80, right: 20, top: 30, bottom: 100}
 
   var circleRadius = 3
   var outerHeight = 500
   var outerWidth = 700
-  var innerHeight = outerHeight - margin.bottom - margin.bottom
-  var innerWidth = outerWidth - margin.right - margin.right
-  console.log(innerWidth)
-  console.log(innerHeight)
+  var innerHeight = outerHeight - margin.top - margin.bottom
+  var innerWidth = outerWidth - margin.left - margin.right
 
-  var xColumn = 'pH'
-  var yColumn = 'alcohol'
+  var xColumn = 'chlorides'
+  var yColumn = 'pH'
   var winetype = 'type'
 
   $('h2').html("Plot: " + xColumn + ' vs ' + yColumn)
+
   // Set up the svg space in the DOM
   var svg = d3.select('body').append('svg')
     .attr("height", outerHeight)
     .attr("width", outerWidth)
 
-  // var g = svg.append('g').attr('tranform', 'translate('+margin.left+','+margin.top+')')
-  var g = svg.append('g').attr('style', 'transform:translate(100,250)')
-  var xAxisG = g.append('g').attr('tranform', 'translate(0,'+innerHeight+')').attr('style','border:double')
-  var yAxisG = g.append('g').attr('style','border:dotted')
+  var g = svg.append('g')
+            .attr('transform','translate('+margin.left+','+margin.top+')')
 
-  /*
   svg.append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
     .attr("x", outerWidth / 2)
     .attr("y", outerHeight - (margin.bottom / 2))
-    .text(xColumn);
+    .text(xColumn.toUpperCase());
 
   svg.append("text")
       .attr("class", "y label")
       .attr("text-anchor", "end")
       .attr("y", margin.top/2)
-      .attr("x", -outerHeight /2)
+      .attr("x", -innerHeight /2)
       .attr("dy", ".75em")
       .attr("transform", "rotate(-90)")
-      .text(yColumn);
-      */
+      .text(yColumn.toUpperCase());
+
   // create the range (pixel length attr) for the plot
   // NOTE: match with size of svg
   var xscale = d3.scale.linear().range([0,innerWidth])
   var yscale = d3.scale.linear().range([innerHeight, 0])
   var colorscale = d3.scale.linear().range(['rgba(0,255,155,0.3)','rgba(255,0,0,0.3)'])
 
-
-  var xAxis = d3.svg.axis().scale(xscale).orient('bottom')
   var yAxis = d3.svg.axis().scale(yscale).orient('left')
+  var xAxis = d3.svg.axis().scale(xscale).orient('bottom')
+
 
   // main fn to build the plt
   function render(data) {
@@ -59,8 +55,14 @@ $(document).ready(function() {
     xscale.domain(d3.extent(data, function (d) {return d[xColumn]}))
     yscale.domain(d3.extent(data, function (d) {return d[yColumn]}))
 
-    xAxisG.call(xAxis);
-    yAxisG.call(yAxis);
+    var xAxisG = g.append('g')
+                  .attr('transform', 'translate(0,'+innerHeight+')')
+                  .attr('class','axis')
+                  .call(xAxis)
+    var yAxisG = g.append('g')
+                  .attr('class','axis')
+                  .call(yAxis)
+
 
     //Bind data points circle svg element
     var circles = g.selectAll('circle').data(data);
--- script2.js
+++ script2.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
 
   // NB: constants
   outerWidth = 200
-  innerWidth = 200
-  margin = {left: 20,right: 40, top: 40, bottom: 40}
+  outerHeight = 200
+  margin = {left: 20,right: 40, top: 30, bottom: 40}
+  innerWidth = outerWidth - margin.left - margin.right
+  innerHeight = outerHeight - margin.top - margin.bottom
+
 
   mock_data = [
     {'x':40,'y':60},
@@ -16,7 +19,15 @@ $(document).ready(function () {
     {'x':110,'y':60},
   ]
 
-for (i in mock_data) {
-  console.log(typeof(mock_data[i].x))
-}
-})
+var svg = d3.select('body').append('svg')
+            .attr('height',outerHeight)
+            .attr('width',outerWidth)
+
+$('svg').css('border','solid')
+
+var g = svg.append('g')
+          .attr('transform','translate('+margin.left+','+margin.top+')')
+
+$('g').html('<text x=20 y=20>hello world!</text>')
+
+});
